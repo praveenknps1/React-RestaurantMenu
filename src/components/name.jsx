6796338403ecac900aa62ae84379a08627c4f637
praveenknps1 <praveenchinnapraveenchinna246@gmail.com>
2024-10-17 12:52:02 +0530

    second commit

---
+++ src/components/name.jsx
@@ -0,0 +1,14 @@
+
+import Desc from "./description";
+
+function Name(){
+    return (
+        <>
+        <h1><center><u>KNPS Restaurant</u></center></h1>
+        <Desc/>
+        </>
+        
+    )
+}
+
+export default Name;
